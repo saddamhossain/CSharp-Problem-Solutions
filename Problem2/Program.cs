406d38d33ba486d428b368dc3579a9956e55c071
saddamhossain <saddamhossain953@gmail.com>
2024-10-16 15:57:41 +0600

    Problem2 added.

---
+++ Problem2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = Subtract(firstNumber, secondNumber);
+
+Console.WriteLine("The subtract is: " + result);
+
+int Subtract(int firstNumber, int secondNumber)
+{
+    return firstNumber - secondNumber;
+}
