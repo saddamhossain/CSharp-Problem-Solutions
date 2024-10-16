b5e4aa1f6014cfd65b8de1512071656f3e279b17
saddamhossain <saddamhossain953@gmail.com>
2024-10-16 15:56:01 +0600

    Problem1 added.

---
+++ Problem1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = Sum(firstNumber, secondNumber);
+
+Console.WriteLine("The sum is: " + result);
+
+int Sum(int firstNumber, int secondNumber)
+{
+    return firstNumber + secondNumber;
+}
