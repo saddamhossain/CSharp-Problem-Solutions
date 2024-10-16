9dde4d926fca87d1a51d4df86c4c32e419c4361e
saddamhossain <saddamhossain953@gmail.com>
2024-10-16 15:59:24 +0600

    Probelm3 added.

---
+++ Problem3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = Multiply(firstNumber, secondNumber);
+
+Console.WriteLine("The subtract is: " + result);
+
+int Multiply(int firstNumber, int secondNumber)
+{
+    return firstNumber * secondNumber;
+}
